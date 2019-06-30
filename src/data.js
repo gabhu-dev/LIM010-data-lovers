aba953cdd1b89ac8d71587974671a3a8ca5281e4
gabhu-dev <gabhu@hotmail.es>
2019-06-30 18:29:36 -0500

    subiendo el login con sus medidas exactas

--- src/data.js
+++ src/data.js
@@ -3,8 +3,11 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
+/*const example = () => {
+for(let i=0;i<LOL.length;i++)
+    return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+const ataque = LOL.filter(LOL => (LOL.attack < 10));
+console.log(equal(ataque))
