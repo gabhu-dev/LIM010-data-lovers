5904b5268e70fca4ff0f37224ffaf44892792666
gabhu-dev <gabhu@hotmail.es>
2019-07-01 23:30:16 -0500

    subiendo arreglos de la primera pantalla

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 //DATOS
-const contrasena =document.getElementById('password');
-const usuario= document.getElementById('usuario');
+const contrasena = document.getElementById('password');
+const usuario = document.getElementById('usuario');
 //BOTONES
 const empezar = document.getElementById('ingresar');
 //VISTAS
-const vistaLogin=document.getElementById('pantalla-login');
-const vistaBienvenida=document.getElementById('pantalla-bienvenida');
+const vistaLogin = document.getElementById('pantalla-login');
+const vistaBienvenida = document.getElementById('pantalla-bienvenida');
 //Contador de intentos en login
 let password = 0;
 //Funciones
-empezar.addEventListener('click', ()=>{
+empezar.addEventListener('click', () => {
     const datos = contrasena.value;
     const datosUsuario = usuario.value;
     if (password < 2) {
-     if(datos==='LABORATORIA' && datosUsuario==='LABORATORIA'){
-         vistaLogin.classList.add('hide');
-         vistaBienvenida.classList.remove('hide');
-     }else{
-         alert('Datos incorrectos,intente otra vez');
-         password++;
-     }        
+        if (datos === 'LABORATORIA' && datosUsuario === 'LABORATORIA') {
+            vistaLogin.classList.add('hide');
+            vistaBienvenida.classList.remove('hide');
+        } else {
+            document.getElementById('mensajeError').innerHTML = 'Datos Incorrectos';
+            password++;
+        }
     }
 });
